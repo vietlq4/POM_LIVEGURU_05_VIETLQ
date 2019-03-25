42dcc46dd28ceef1b659b99dd07cd675dbf2d8fa
vietlq4 <like.mrtun@gmail.com>
2019-03-25 12:27:08 +0700

    change pom.xml

--- src/main/java/interfaces/pagesUI/AbstractPageUI.java
+++ src/main/java/interfaces/pagesUI/AbstractPageUI.java
@@ -7,7 +7,7 @@ public class AbstractPageUI {
 	public static final String DYNAMIC_TEXTBOX = "//input[@name='%s']";
 	public static final String DYNAMIC_HEADER_ACCOUNT= "//div[@id='header-account']//a[@title='%s']";
 	public static final String DYNAMIC_LABEL= "//span[@class='label' and text()='%s']";
-	public static final String DYNAMIC_LINK_ON_SIDEBAR= "//div[@class='col-left sidebar col-left-first']//span[text()='%s']";
+	public static final String DYNAMIC_LINK_ON_SIDEBAR = "//div[@class='col-left sidebar col-left-first']//span[text()='%s']";
 	public static final String DYNAMIC_PRODUCT_IMAGE= "//a[@title='%s']/img";
 	public static final String DYNAMIC_PRODUCT_NAME= "//h2[@class='product-name']/a[@title='%s']";
 	public static final String DYNAMIC_PRODUCT_PRICE= "//h2[@class='product-name']/a[@title='%s']/parent::h2/following-sibling::div[@class='price-box']//span[@class='price']" ;
